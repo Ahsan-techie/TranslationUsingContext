7b9c1deb1e2a98d856ef9919fac5d6d6ab1c5dbd
ahsan <unknownuser34754@gmail.com>
2020-12-08 21:17:50 +0500

    Use context to change languages

---
+++ src/components/App.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import UserCreate from './UserCreate'
+import LanguageContext from '../contexts/LanguageContext'
+
+
+class App extends React.Component {
+    state={language:'english'}
+    onLanguageChange= language=>{
+        
+            this.setState({language})
+        
+    }
+    render(){
+    return (
+        <div className='ui container'>
+            <div>
+            Select a Language:
+                <i className="flag us" onClick={()=>this.onLanguageChange('english')}/>
+                <i className="flag pk" onClick={()=>this.onLanguageChange('urdu')}/>
+            </div>
+            <LanguageContext.Provider value={this.state.language}>
+            <UserCreate /> </LanguageContext.Provider>
+        </div>
+    )}
+}
+
+export default App
---
+++ src/components/Field.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import LanguageContext from '../contexts/LanguageContext'
+
+export class Field extends Component {
+    static contextType=LanguageContext
+    render() {
+        const  text = this.context === "english"? "Name" : "نام"
+        return (
+            <div className='ui field'>
+                <label>{text}</label>
+                <input />
+            </div>
+        )
+    }
+}
+
+export default Field
